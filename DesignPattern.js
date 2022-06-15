5f9cc55aa03f363bca26597a4939672319ed217b
Zuber-GitHub <zuberriyazahmed@gmail.com>
2022-06-15 13:18:55 +0530

    Check_eligibility

--- DesignPattern.js
+++ DesignPattern.js
@@ -1,29 +1,68 @@
 class Student
 {   static studentNum = 0
+    static studentLst = []
+
     constructor(name,age,phoneNumber,marks)
-    {
+
+
+    {   this.minMarks = 40
+        this.eligibleAge = 18
         this.name  = name
         this.age = age
         this.phoneNumber = phoneNumber
         this.marks = marks
         Student.studentNum+=1
+        let count = Student.studentNum
+        this.studentObj ={}
+        this.studentObj[count] = this.name
+        Student.studentLst.push(this.studentObj)
+        
+        
     }
     checkEligibility()
     {
-        if(this.marks>=40) console.log(`${this.name} is eligible for palcements`);
-        else console.log(`${this.name} is not eligible for placements`);
+        return ()=>{
+            if(this.age>this.eligibleAge && this.marks>this.minMarks)
+            {
+                console.log(`${this.name} is eligible for placements`)
+            
+                // console.log(Student.studentLst)
+            }
+            else (console.log(`${this.name} is not eligible for placements`))
+            
+        }
     }
+    
     static checkStudentNums()
     {   
         console.log(`Number of registered students is ${Student.studentNum}`)
     }
+    
+    static displayEligibleLst()
+    {   
+        var x = checkEligibility()
+        for(let i of Student.studentLst)
+        {
+           
+        }
+       
+}
 }
 const Student_01 = new Student('Sourab',23,797522211,35)
 const Student_02 = new Student('Vinay',24,897522211,41)
 const Student_03 = new Student('Vineet',24,997522211,75)
 const Student_04 = new Student('Ashutosh',24,697522211,58)
-const Student_05 = new Student('Mahesh',24,887522211,33)
-Student_02.checkEligibility()
-Student.checkStudentNums()
+const Student_05 = new Student('Mahesh',24,887522211,60)
+Student_05.checkEligibility()()
+//Student.displayEligibleLst()
+
+
+
+
+
+
+
+
+
 
 
