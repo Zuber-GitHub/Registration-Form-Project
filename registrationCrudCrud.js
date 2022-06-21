bfb0743dbec6ec178a86f6e8173d808bce7df21a
Zuber-GitHub <zuberriyazahmed@gmail.com>
2022-06-21 14:18:36 +0530

    getUserDetails

--- registrationCrudCrud.js
+++ registrationCrudCrud.js
@@ -1,3 +1,17 @@
+document.addEventListener('DOMContentLoaded', reload)
+function reload(e)
+{   
+
+    axios.get("https://crudcrud.com/api/63249559f9554d179385aae28cdf8f06/userDetails").
+    then(displayData =>
+        {
+            for(let i=0; i<displayData.data.length; i++)
+            {
+                showOutput(displayData.data[i])
+            }
+        })
+}
+
 let getCallBtn = document.getElementById('clicked')
 getCallBtn.addEventListener('click', postUserData)
 function postUserData(event01)
@@ -9,58 +23,59 @@ function postUserData(event01)
     let canPhone = document.getElementById('phone').value
     let canTime = document.getElementById('birthdaytime').value
     let userObj = {"Name": canName, "Email":canEmail,"Phone":canPhone,"Time":canTime}
-    console.log(userObj)
     function addUserToCrudCrud()
     {
         axios.post("https://crudcrud.com/api/63249559f9554d179385aae28cdf8f06/userDetails", userObj)
     }
     addUserToCrudCrud()
+    showOutput(userObj)
 
-    function showOutput()
-    {
-        
-        let mainClass = document.getElementById('users')
-        let childClass = document.createElement('li')
-        childClass.textContent = `${userObj.Name}  -  ${userObj.Email}  -  ${userObj.Phone}  -  ${userObj.Time}`
-        mainClass.append(childClass)
-        let editBtn = document.createElement('button')
-        editBtn.textContent = 'Edit'
-        editBtn.style.backgroundColor = 'green'
-        childClass.appendChild(editBtn)
-        let delBtn = document.createElement('button')
-        delBtn.textContent = 'Delete'
-        delBtn.style.backgroundColor = 'red'
-        childClass.appendChild(delBtn)
-        delBtn.addEventListener('click', ()=>
-        {
-            if(confirm('Delete User Info?'))
-                {
-                    mainClass.removeChild(childClass)
-                    axios.delete("https://crudcrud.com/api/63249559f9554d179385aae28cdf8f06/userDetails")
-                    
-                }
-                 }
-                 )
-        editBtn.addEventListener('click', ()=>
-        {   
-                    
-            
-            let newName = prompt('Add Name')
-            let newEmail = prompt('Add Email')
-            let newPhone = prompt('Add Phone Number')
-            let newTime = prompt('Add Meeting Time','YYYY-MM-DD HH:MM')
-            childClass.textContent = `${newName}  -  ${newEmail}  -  ${newPhone}  -  ${newTime}`          
-            let obj01 = {Name:newName,Email:newEmail,Phone:newPhone,Time:newTime}
-            childClass.append(delBtn)
-            childClass.appendChild(editBtn)
-
-
-                 })
-             }
-
-    
-    showOutput()
 }
+
+function showOutput(obj)
+{
+    
+    let mainClass = document.getElementById('users')
+    let childClass = document.createElement('li')
+    childClass.textContent = `${obj.Name}  -  ${obj.Email}  -  ${obj.Phone}  -  ${obj.Time}`
+    mainClass.append(childClass)
+    let editBtn = document.createElement('button')
+    editBtn.textContent = 'Edit'
+    editBtn.style.backgroundColor = 'green'
+    childClass.appendChild(editBtn)
+    let delBtn = document.createElement('button')
+    delBtn.textContent = 'Delete'
+    delBtn.style.backgroundColor = 'red'
+    childClass.appendChild(delBtn)
+    delBtn.addEventListener('click', ()=>
+    {
+        if(confirm('Delete User Info?'))
+            {
+                mainClass.removeChild(childClass)
+                
+                
+            }
+             }
+             )
+    editBtn.addEventListener('click', ()=>
+    {   
+                
+        
+        let newName = prompt('Add Name')
+        let newEmail = prompt('Add Email')
+        let newPhone = prompt('Add Phone Number')
+        let newTime = prompt('Add Meeting Time','YYYY-MM-DD HH:MM')
+        childClass.textContent = `${newName}  -  ${newEmail}  -  ${newPhone}  -  ${newTime}`          
+        let obj01 = {Name:newName,Email:newEmail,Phone:newPhone,Time:newTime}
+        childClass.append(delBtn)
+        childClass.appendChild(editBtn)
+
+
+             })
+         }
+
+
+
 
 
 
